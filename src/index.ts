65ae8768b1939021ed56de8f74b214eca40ab7e6
Bryce <bryce@brycedev.com>
2023-03-07 21:39:08 -0500

    add benchmark type selection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export const one = 1
+import { createSelection } from 'bun-promptx'
+
+const result = createSelection([
+  { text: 'cpu', description: 'benchmark cpu' },
+  { text: 'acc', description: 'attempt to benchmark software acceleration' },
+  { text: 'gpu', description: 'attempt to benchmark gpu' },
+  { text: 'exhaustive', description: 'run cpu + acc + gpu benchmarks' },
+], {
+  headerText: 'select benchmark type',
+  perPage: 5,
+})
+
+console.log(result)
